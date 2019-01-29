49b01f24a2c2450deb76420b0741a8be6e464a77
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-29 15:53:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist= { "Green Day": "American Idiot"  };
 
 function updatePlaylist(playlist, artistName,songTitle){
-  playlist["Phil Ochs"]= "My Bloody Valentine"
+
   return updatePlaylist
 }
 
